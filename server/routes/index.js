577e5cc00491206f1e7e243f9d32b78f0092b838
Patrick <pcnicklaus@gmail.com>
2015-12-02 22:48:06 -0700

    this is one of those bad ones where you can't remember whtacha did and you somehow in the midst of your fourteen hour day of coding, for some god forsaken reason, probably you were just gonna have a quick look at something, you forgot to create a branch... yeah, that's this commit.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,6 @@ var request   = require('request');
 
 router.get('/scrape', function(req, res, next) {
     // var newDater = new Dater({
-    //   username: req.body.username,
-    //   gender: req.body.gender,
-    //   age: req.body.age,
     //   location: req.body.location
     // });
 
@@ -30,14 +27,6 @@ router.get('/scrape', function(req, res, next) {
                 link = data.attr('href');
                 holder = title + ' ' + link;
                 jobs.push(holder);
-                // console.log(title, link, 'link');
-                // link = data.parent().text();
-                // console.log(link)
-                // test = data.parent().parent().text
-                // console.log(test)
-                // title = data.children().first().text();
-
-                // json.title = title;
             });
             console.log(jobs);
             res.send(jobs);
@@ -50,35 +39,42 @@ router.post('/detail', function(req, res, next) {
     url = req.body.url;
     console.log(url)
 
-    // request(url, function(error, response, html){
-    //     if(!error){
-    //         var $ = cheerio.load(html);
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
 
-    //         var title;
-    //         var link;
-    //         var holder;
-    //         var jobs = [];
+            var jobDetail = {
+                title: '',
+                description: '',
+                compensation: '',
+                contactInfo: {
+                    phone: null,
+                    email: null
+                },
+                imageUrl: null
+            };
 
-    //         $('a.hdrlnk').filter(function(){
-    //             var data = $(this);
-    //             // console.log(data, data.attr('href'));
-    //             title = data.text();
-    //             link = data.attr('href');
-    //             holder = title + ' ' + link;
-    //             jobs.push(holder);
-    //             // console.log(title, link, 'link');
-    //             // link = data.parent().text();
-    //             // console.log(link)
-    //             // test = data.parent().parent().text
-    //             // console.log(test)
-    //             // title = data.children().first().text();
+            $('span.postingtitletext').filter(function(){
+                var data = $(this);
+                console.log(data.text());
+                jobDetail.title = data;
 
-    //             // json.title = title;
-    //         });
-    //         console.log(jobs);
-    //         res.send(jobs);
-    //     }
-    // });
+                // title = data.text();
+                // link = data.attr('href');
+                // holder = title + ' ' + link;
+                // jobs.push(holder);
+                // console.log(title, link, 'link');
+                // link = data.parent().text();
+                // console.log(link)
+                // test = data.parent().parent().text
+                // console.log(test)
+                // title = data.children().first().text();
+
+                // json.title = title;
+            });
+            // res.send(jobs);
+        }
+    });
 });
 
 module.exports = router;
